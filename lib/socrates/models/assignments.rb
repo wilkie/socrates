6c0220f089f920d58dd8adb35ef2f1e445c64cae
wilkie <wilkie05@gmail.com>
2011-01-29 01:23:32 -0500

    Adding Assignments data and model.

--- lib/socrates/models/assignments.rb
+++ lib/socrates/models/assignments.rb
@@ -1,2 +1,26 @@
+require 'yaml'
+
 class Assignments
+	def self.instance
+		@@last_loaded
+	end
+
+	def initialize(configuration_file)
+		@@last_loaded = self
+		@assignments = YAML.load_file(configuration_file)
+	end
+
+	def types
+		@assignments.keys
+	end
+
+	def list(type)
+		@assignments[type].map do |assignment|
+			result = {}
+			result[:title] = assignment["title"]
+			result[:href] = assignment["href"]
+			result[:description] = assignment["description"]
+			result
+		end
+	end
 end
