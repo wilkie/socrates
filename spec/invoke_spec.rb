6cf02213f637a0ba3a6ff651608d1487c03fd17b
wilkie <wilkie05@gmail.com>
2011-01-29 18:39:19 -0500

    Added more tests and cleaned up specification.

--- spec/invoke_spec.rb
+++ spec/invoke_spec.rb
@@ -4,27 +4,45 @@ require_relative '../lib/socrates.rb'
 
 describe Socrates do
 	describe "#invoke" do
-		it "should generate an invocation.yml file in the specified directory" do
+		before(:each) do
 			if File.exists?('test/invocation.yml')
 				File.delete('test/invocation.yml')
 			end
+			if File.exists?('test/socrates.yml')
+				File.delete('test/socrates.yml')
+			end
+		end
+
+		after(:each) do
+			if File.exists?('test/invocation.yml')
+				File.delete('test/invocation.yml')
+			end
+			if File.exists?('test/socrates.yml')
+				File.delete('test/socrates.yml')
+			end
+		end
+		
+		it "should generate a socrates.yml file in the specified directory" do
+			Socrates.invoke('test/course', 'test')
+			File.exists?('test/socrates.yml').should eql(true)
+		end
+
+		it "should generate a socrates.yml file in the current directory if no destination is given" do
+			Dir.chdir('test')
+			Socrates.invoke('test/course')
+			File.exists?('socrates.yml').should eql(true)
+			Dir.chdir('..')
+		end 
+
+		it "should generate an invocation.yml file in the specified directory" do
 			Socrates.invoke('test/course', 'test')
 			File.exists?('test/invocation.yml').should eql(true)
-			if File.exists?('test/invocation.yml')
-				File.delete('test/invocation.yml')
-			end
 		end
 
 		it "should generate an invocation.yml file in the current directory if no destination is given" do
 			Dir.chdir('test')
-			if File.exists?('invocation.yml')
-				File.delete('invocation.yml')
-			end
 			Socrates.invoke('test/course')
 			File.exists?('invocation.yml').should eql(true)
-			if File.exists?('invocation.yml')
-				File.delete('invocation.yml')
-			end
 			Dir.chdir('..')
 		end
 	end
